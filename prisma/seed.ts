3ca26725f699318e16fc89788f5b4d7c69e69f5f
Mads E. Kirchhoff <mek@minuba.dk>
2022-03-06 00:06:17 +0100

    Setup database and Prisma ORM

---
+++ prisma/seed.ts
@@ -0,0 +1,30 @@
+import {PrismaClient} from "@prisma/client";
+
+const db = new PrismaClient();
+
+async function seed() {
+    await Promise.all(
+        seedTenancies().map((tenancy) => {
+            return db.tenancy.create({data: tenancy});
+        })
+    );
+}
+
+seed();
+
+function seedTenancies() {
+    return [
+        {
+            address: "Bogholder Allé 46A",
+            size: "78",
+            rooms: "2",
+            description: "Decent beginner apartment.",
+        },
+        {
+            address: "Somewhere, over the rainbow 5",
+            size: "185",
+            rooms: "6",
+            description: "The sun will shine.",
+        },
+    ]
+}
