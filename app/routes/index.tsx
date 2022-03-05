7ff27dff4ef879ce8d261e25a134ab249ec554f6
Mads E. Kirchhoff <mek@minuba.dk>
2022-03-05 22:34:52 +0100

    Initial Remix scaffold

---
+++ app/routes/index.tsx
@@ -0,0 +1,9 @@
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Welcome Tarnished</h1>
+      <ul>
+      </ul>
+    </div>
+  );
+}
