64e6c2a90c86a587833855feae554b3e4a273ca1
Mads E. Kirchhoff <mek@minuba.dk>
2022-03-15 22:20:31 +0100

    Basic back-end communication and loading tenancies from DB done (yes, it's that easy in Remix)

--- app/routes/Portfolio.tsx
+++ app/routes/Portfolio.tsx
@@ -1,10 +1,20 @@
-const Portfolio = (): JSX.Element => {
+import type {LoaderFunction} from "remix"
+import {useLoaderData} from "remix"
+import {db} from "~/utils/db.server"
+import type {Tenancy} from "@prisma/client"
+import {Button, Text} from "@nextui-org/react"
+
+export default function Portfolio() {
+    // This is a remix hook that automatically loads data from serverside, aka the "loader" function below
+    const data = useLoaderData<Tenancy[]>();
     return (
-        <div style={{fontFamily: "system-ui, sans-serif", lineHeight: "1.4"}}>
-            <h1>Welcome Tarnished</h1>
-            <ul>
-            </ul>
-        </div>
-    );
+        <>
+            <Text h1 size={60} weight="bold" css={{textGradient: '90deg, $green900 -20%, $green200 50%'}}>Welcome Proper</Text>
+            <form method="post">
+                {data?.map(tenancy => tenancy.address)}</form>
+            <Button>Add Tenancy</Button>
+        </>
+    )
 }
-export default Portfolio
+
+export let loader: LoaderFunction = async () => db.tenancy.findMany()
