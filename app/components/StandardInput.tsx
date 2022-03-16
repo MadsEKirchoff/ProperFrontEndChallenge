b15d44ac20293447f09802817967378ab92ec99f
Mads E. Kirchhoff <mek@minuba.dk>
2022-03-16 02:55:22 +0100

    Pull out tenancy to it's own component and first crack at saving (works too well)

--- app/components/StandardInput.tsx
+++ app/components/StandardInput.tsx
@@ -1,11 +1,12 @@
 import {Input} from '@nextui-org/react'
 
 type StandardInputProps = {
-    value: string,
-    labelPlaceholder: string
+    value?: string,
+    labelPlaceholder: string,
+    name: string
 }
 
 /** A small input wrapper just to standardize border  **/
-export default function StandardInput({value, labelPlaceholder}: StandardInputProps) {
-    return <Input value={value} labelPlaceholder={labelPlaceholder} bordered  clearable/>
+export default function StandardInput({value, labelPlaceholder, name}: StandardInputProps) {
+    return <Input value={value} labelPlaceholder={labelPlaceholder} name={name} bordered clearable/>
 }
